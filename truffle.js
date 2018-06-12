31bf5ac4d2e92141a3632b56137d17477c7de783
Jalal Albasri <jalal.albasri@gmail.com>
2018-06-12 12:48:15 +0300

    ganache

--- truffle.js
+++ truffle.js
@@ -1,9 +1,9 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      host: 'localhost',
+      port: 7545,
+      network_id: '*' // Match any network id
     }
   }
 };
